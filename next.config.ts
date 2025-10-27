c7793fff74ed2c9959fd1ffa63c24aefa6b003c9
tetsup <58599705+tetsup@users.noreply.github.com>
2025-10-28 08:44:10 +0900

    setup

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  env: {
+    API_URL: process.env.API_URL,
+    MONGO_ADDR: process.env.MONGO_ADDR,
+    MONGO_USER: process.env.MONGO_USER,
+    MONGO_PASS: process.env.MONGO_PASS,
+    MONGO_DATABASE: process.env.MONGO_DATABASE,
+  },
 };
 
 export default nextConfig;
